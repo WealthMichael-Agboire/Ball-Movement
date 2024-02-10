f9d144559bd9517525f330a4058514a7141ea84d
WealthMichael-Agboire <wealthmichaelagboire@gmail.com>
2024-02-10 13:14:03 -0500

    IN CLASS ASSIGNMENT

---
+++ class_02_10_24/Assets/Scripts/Ball_Movements.cs
@@ -0,0 +1,27 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Movements : MonoBehaviour
+{
+ //varibles for game
+ public float xspeed = 0.5f; //variable for the movement on the x axis
+  public float xBorder = 7.5f;
+        public bool xMove = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        xspeed = 0.01f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (xMove == true) {
+       transform.position = new Vector2 (transform.position.x + xspeed, transform.position.y);
+        }
+        if (transform.position.x>=xBorder) {
+            xMove=false;
+    }
+    }
+}
