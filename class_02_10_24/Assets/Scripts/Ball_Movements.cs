89176961d07a68bed10b19b20817c3fde75215ba
WealthMichael-Agboire <84628671+WealthMichael-Agboire@users.noreply.github.com>
2024-02-13 01:00:27 -0500

    Test

--- class_02_10_24/Assets/Scripts/Ball_Movements.cs
+++ class_02_10_24/Assets/Scripts/Ball_Movements.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Ball_Movements : MonoBehaviour
 {
- //varibles for game
- public float xspeed = 0.5f; //variable for the movement on the x axis
-  public float xBorder = 7.5f;
-    public float yspeed = 0.5f;
+    //varibles for game
+    public float xspeed = 0.01f; //variable for the movement on the x axis
+    public float xBorder = 7.5f;
+    public float yspeed = 0.01f;
     public float yBorder = 7.5f;
     public bool yMove = true;
     public bool xMove = true;
@@ -15,18 +15,20 @@ public class Ball_Movements : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        xspeed = 0.01f;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (xMove == true) {
-       transform.position = new Vector2 (transform.position.x + xspeed, transform.position.y);
+        if (xMove == true)
+        {
+            transform.position = new Vector2(transform.position.x + xspeed, transform.position.y);
         }
-        if (transform.position.x>=xBorder) {
-            xMove=false;
-    }
+        if (transform.position.x >= xBorder)
+        {
+            xMove = false;
+        }
         if (yMove == true)
         {
             transform.position = new Vector2(transform.position.x, transform.position.y + yspeed);
@@ -35,6 +37,22 @@ public class Ball_Movements : MonoBehaviour
         {
             yMove = false;
         }
+        if (xMove == true)
+        {
+            transform.position = new Vector2(transform.position.x - xspeed, transform.position.y);
+        }
+        if (transform.position.x < xBorder)
+        {
+            xMove = false;
+        }
 
+        if (yMove == true)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - yspeed);
+        }
+        if (transform.position.y < yBorder)
+        {
+            yMove = false;
+        }
     }
 }
