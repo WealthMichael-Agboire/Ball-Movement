c32e06d749504639ce1c35a064987c247740a961
WealthMichael-Agboire <84628671+WealthMichael-Agboire@users.noreply.github.com>
2024-02-13 18:10:03 -0500

    Update Ball_Movements.cs

--- class_02_10_24/Assets/Scripts/Ball_Movements.cs
+++ class_02_10_24/Assets/Scripts/Ball_Movements.cs
@@ -8,7 +8,7 @@ public class Ball_Movements : MonoBehaviour
     public float xspeed = 0.01f; //variable for the movement on the x axis
     public float xBorder = 7.5f;
     public float yspeed = 0.01f;
-    public float yBorder = 7.5f;
+    public float yBorder = 4.2f;
     public bool yMove = true;
     public bool xMove = true;
 
@@ -37,22 +37,6 @@ public class Ball_Movements : MonoBehaviour
         {
             yMove = false;
         }
-        if (xMove == true)
-        {
-            transform.position = new Vector2(transform.position.x - xspeed, transform.position.y);
-        }
-        if (transform.position.x < xBorder)
-        {
-            xMove = false;
-        }
-
-        if (yMove == true)
-        {
-            transform.position = new Vector2(transform.position.x, transform.position.y - yspeed);
-        }
-        if (transform.position.y < yBorder)
-        {
-            yMove = false;
-        }
+       
     }
 }
