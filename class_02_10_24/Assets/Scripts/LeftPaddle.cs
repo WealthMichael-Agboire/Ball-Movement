b0d0ed3316834c10ab86f56029b58deefbd62729
WealthMichael-Agboire <84628671+WealthMichael-Agboire@users.noreply.github.com>
2024-02-17 21:50:44 -0500

    Update

--- class_02_10_24/Assets/Scripts/LeftPaddle.cs
+++ class_02_10_24/Assets/Scripts/LeftPaddle.cs
@@ -2,42 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PaddleMovement : MonoBehaviour
-{
+public class LeftPaddle : MonoBehaviour
 
-   public float speed = 10f;
+{
+    public float speed = 10f;
    private Rigidbody2D rb2d;
    public float yBorder = 2.5f;
     // Start is called before the first frame update
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>(); //declare value of the variable
+         rb2d = GetComponent<Rigidbody2D>(); //declare value of the variable 
     }
 
     // Update is called once per frame
     void Update()
     {
-        var velocity = rb2d.velocity; //creates and declare variables for velocity
+            var velocity = rb2d.velocity; //creates and declare variables for velocity
     
     //control the paddle
-    if (Input.GetKey(KeyCode.O)  && transform.position.y <= yBorder)
+    if (Input.GetKey(KeyCode.W)  && transform.position.y <= yBorder)
     { 
 velocity.y = speed; //move the speed 
-Debug.Log("O");
+Debug.Log("W");
     }
-    else if (Input.GetKey(KeyCode.L) && transform.position.y >= -yBorder)
+    else if (Input.GetKey(KeyCode.A) && transform.position.y >= -yBorder)
     {
         velocity.y = -speed; //move the speed down
-        Debug.Log("L");
+        Debug.Log("A");
     }
     else { //otherwise
 velocity.y = 0; //dont move
     }
 rb2d.velocity = velocity;
-    
-    
-   
     }
-    
-    }
-
+}
